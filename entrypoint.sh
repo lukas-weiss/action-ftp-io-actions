35f1f24da88bcc74e9f48e10fd9451f8bd12d74e
Lukas Weiß <lukas.weiss@exxeta.com>
2022-01-30 19:45:50 +0100

    Adapt mirror params to be faster

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,4 +5,4 @@ echo "==== Do action $INPUT_FTP_IO_ACTION ===="
 lftp --debug \
   -u "$INPUT_FTP_USER","$INPUT_FTP_PASSWORD" \
   "$INPUT_FTP_SERVER" \
-  -e "set ssl:verify-certificate no; set ftp:ssl-allow no; mirror --verbose $([ "$INPUT_FTP_IO_ACTION" == 'upload' ] && echo "--reverse") $([ "$INPUT_DRY_RUN" == true ] && echo "--dry-run") $INPUT_SOURCE_FOLDER $INPUT_TARGET_FOLDER .; quit;"
+  -e "set ssl:verify-certificate false; mirror --parallel=8 $([ "$INPUT_FTP_IO_ACTION" == 'download' ] && echo "--dereference") $([ "$INPUT_FTP_IO_ACTION" == 'upload' ] && echo "--reverse") $([ "$INPUT_DRY_RUN" == true ] && echo "--dry-run") $INPUT_SOURCE_FOLDER $INPUT_TARGET_FOLDER; exit;"
